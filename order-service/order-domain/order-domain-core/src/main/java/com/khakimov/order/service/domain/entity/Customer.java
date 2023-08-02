69e4ea94973d2d582adcb61ef1a5b9059801c8b6
AndreyKhakimov <khakimov013@gmail.com>
2023-08-02 16:51:50 +0200

    Add order state changing methods, Customer and Restaurant entities

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/khakimov/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.khakimov.order.service.domain.entity;
+
+import com.khakimov.domain.entity.AggregateRoot;
+import com.khakimov.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
